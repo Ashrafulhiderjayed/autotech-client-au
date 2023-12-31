a3364a10e7a0a8302b05509b450f70e86012b22d
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2023-12-31 22:25:35 +0600

    sign up

--- src/Routes/Reoutes.jsx
+++ src/Routes/Reoutes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
     {
@@ -13,8 +14,12 @@ const router = createBrowserRouter([
           element: <Home></Home>
         },
         {
-          path: '/login',
+          path: 'login',
           element: <Login></Login>
+        },
+        {
+          path: 'signup',
+          element: <SignUp></SignUp>
         },
       ]
     },
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,23 +1,39 @@
+// import { NavLink } from 'react-router-dom';
 import { FaFacebookF } from "react-icons/fa";
 import { IoLogoGoogleplus } from "react-icons/io";
 import { FaLinkedinIn } from "react-icons/fa";
-import './Login.css'
+import './SignUp.css'
 import { NavLink } from "react-router-dom";
 
-const Login = () => {
+const SignUp = () => {
     return (
         <section className="hero min-h-screen bg-base-200">
-            <div className="flex flex-col lg:flex-row shadow-xl hover:shadow-lg">
-                {/* login div  */}
+            <div className="flex flex-col lg:flex-row-reverse shadow-xl hover:shadow-lg">
+                {/* without signUp div  */}
+                <div id='changed-item-vertically' className="lg:flex-1 px-5 order-1 lg:order-2 bg-gradient-to-r from-[#ff2b2b] to-[#FF416C] text-white text-center w-96 items-center justify-center">
+                    <h2 className="items-center text-5xl text-center font-bold pt-8">Welcome Back!</h2>
+                    <p className="my-4">Are you have created account before? If you are already a valid user. Please sign in</p>
+                    <NavLink to="/login">
+                        <button className="btn bg-transparent border-white text-white font-bold w-40 my-6 rounded-3xl shadow-2xl hover:bg-black">SIGN IN</button>
+                    </NavLink>
+                </div>
+
+                {/* sign up div  */}
                 <div className="lg:flex-1 p-1 order-2 lg:order-1 bg-white w-96">
-                    <h2 className="text-center text-5xl font-bold pt-10">Sign in</h2>
+                    <h2 className="text-center text-5xl font-bold pt-8">Sign up</h2>
                     <span className="flex align-middle justify-center mt-7">
                         <FaFacebookF className="border border-slate-400 rounded-full text-xl p-2 shadow-md hover:shadow-transparent box-content" />
                         <IoLogoGoogleplus className="border border-slate-400 rounded-full text-xl p-2 shadow-md hover:shadow-transparent box-content mx-4" />
                         <FaLinkedinIn className="border border-slate-400 rounded-full text-xl p-2 shadow-md hover:shadow-transparent box-content" />
                     </span>
-                    <p className="text-zinc-600 text-center pt-2">or use your account</p>
+                    <p className="text-zinc-600 text-center pt-2">or use your email for registration</p>
                     <form className="card-body">
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" placeholder="Name" className="input bg-slate-100" required />
+                        </div>
                         <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
@@ -33,24 +49,15 @@ const Login = () => {
                             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                         </label>
                         </div>
-                        <div className="form-control mt-6">
-                            <NavLink to="/signup">
-                            <button className="btn bg-loginOrangeColor text-white font-bold w-3/5 px-2 ml-16 rounded-3xl border-none shadow-2xl hover:shadow-xl hover:bg-black">SIGN IN</button>
-                            </NavLink>
+                        <div className="form-control mt-6">                        
+                            <button className="btn bg-loginOrangeColor text-white font-bold w-3/5 px-2 ml-16 rounded-3xl border-none shadow-2xl hover:shadow-xl hover:bg-black">SIGN UP</button>
                         </div>
                     </form>
                 </div>
-                {/* right div  */}
-                <div id='changed-item-vertically' className="lg:flex-1 px-5 order-1 lg:order-2 bg-gradient-to-r from-[#ff2b2b] to-[#FF416C] text-white text-center w-96 items-center justify-center">
-                    <h2 className="items-center text-5xl text-center font-bold pt-8">Hello Users!</h2>
-                    <p className="my-4">Exclusive discount for new members only, join before it's too late!</p>
-                    <NavLink to="/signup">
-                        <button className="btn bg-transparent border-white text-white font-bold w-40 my-6 rounded-3xl shadow-2xl hover:bg-black">SIGN UP</button>
-                    </NavLink>
-                </div>
+                
             </div>
         </section>
     );
 };
 
-export default Login;
+export default SignUp;
